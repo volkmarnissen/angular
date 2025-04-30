28ce7382682f2aa776decb9ace2161c9eab83271
volkmarnissen <volkmar.nissen@gmail.com>
2025-04-30 21:13:31 +0200

    select slaves

--- cypress/select-modbus.cy.ts
+++ cypress/select-modbus.cy.ts
@@ -5,6 +5,9 @@ describe("Select Slave tests", () => {
   beforeEach(beforeEachHelper); // mounts entity and opens all expansion panels
   //afterEach(afterEachEntityHelper);
   it("mount", () => {
+    cy.intercept("GET", "**/converters", {
+      fixture: "converters.json",
+    });
     cy.get('mat-select[formControlName="variableType"]')
       });
   });
