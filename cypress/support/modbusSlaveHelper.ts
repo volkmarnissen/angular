df8efc9c35905741ee596aa2551caa32338ec637
volkmarnissen <volkmar.nissen@gmail.com>
2025-04-30 21:13:15 +0200

    Start testing select-slave

--- cypress/support/modbusSlaveHelper.ts
+++ cypress/support/modbusSlaveHelper.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute, provideRouter } from "@angular/router";
 import { apiUri } from "@modbus2mqtt/server.shared";
 import { SelectSlaveComponent } from "angular/src/app/select-slave/select-slave.component";
 import { from } from "rxjs";
-let ev = new EventEmitter<number | undefined>()
+let ev = new EventEmitter<number | undefined>();
 // entityApiService.getConfiguration {mqttbasetopic rootUrl apiUri.configuration
 // entityApiService.getBus  bus.connectionData, bus.busId apiUri.bus
 // entityApiService.getSlaves Islave[] apiUri.slaves
@@ -16,73 +16,82 @@ let ev = new EventEmitter<number | undefined>()
 // not implemented yet deleteSlave
 // not implemented yet postSlave
 
-var _slaveAdded= false
-export function slaveAdded(){_slaveAdded= true}
+var _slaveAdded = false;
+export function slaveAdded() {
+  _slaveAdded = true;
+}
 
-function buildReply():any{
-  if(_slaveAdded)
-  return {
-    statusCode: 202,
-    delay: 100,
-    body:[
-      {
-          "filename":  "s2",
-          "name": "Slave 2",
-          "status":  0,
-          "entities":  [{
-              "id": 1,
-              "name": "entity1",
-              "readonly": true,
-              "mqttname": "e1"
-          }],
-          "identified":  1
-      },
-      {
-        "filename":  "s2second",
-        "name": "S2 Second",
-        "status":  0,
-        "entities":  [{
-            "id": 1,
-            "name": "s2.second.entity1",
-            "readonly": true,
-            "mqttname": "s2e1"
-        }],
-        "identified":  1
-    },
-    
-   ]
-  };
-  else return {
-    statusCode: 202,
-    delay: 100,
-    body:[
-      {
-          "filename":  "dimplexpco5",
-          "name": "Dimplex Heat Pump",
-          "status":  0,
-          "entities":  [{
-              "id": 1,
-              "name": "entity1",
-              "readonly": true,
-              "mqttname": "e1"
-          }],
-          "identified":  1
-      },
-      {
-        "filename":  "second",
-        "name": "Second",
-        "status":  0,
-        "entities":  [{
-            "id": 1,
-            "name": "second.entity1",
-            "readonly": true,
-            "mqttname": "se1"
-        }],
-        "identified":  1
-    },
-    
-   ]
-  }
+function buildReply(): any {
+  if (_slaveAdded)
+    return {
+      statusCode: 202,
+      delay: 100,
+      body: [
+        {
+          filename: "s2",
+          name: "Slave 2",
+          status: 0,
+          entities: [
+            {
+              id: 1,
+              name: "entity1",
+              readonly: true,
+              mqttname: "e1",
+            },
+          ],
+          identified: 1,
+        },
+        {
+          filename: "s2second",
+          name: "S2 Second",
+          status: 0,
+          entities: [
+            {
+              id: 1,
+              name: "s2.second.entity1",
+              readonly: true,
+              mqttname: "s2e1",
+            },
+          ],
+          identified: 1,
+        },
+      ],
+    };
+  else
+    return {
+      statusCode: 202,
+      delay: 100,
+      body: [
+        {
+          filename: "dimplexpco5",
+          name: "Dimplex Heat Pump",
+          status: 0,
+          entities: [
+            {
+              id: 1,
+              name: "entity1",
+              readonly: true,
+              mqttname: "e1",
+            },
+          ],
+          identified: 1,
+        },
+        {
+          filename: "second",
+          name: "Second",
+          status: 0,
+          entities: [
+            {
+              id: 1,
+              name: "second.entity1",
+              readonly: true,
+              mqttname: "se1",
+            },
+          ],
+          identified: 1,
+        },
+      ],
+    };
 }
 /**
  * mounts the specification-entity-component and opens all expansion panels
@@ -94,25 +103,29 @@ function buildReply():any{
  * If other initial values are required, a new test file is required
  */
 export function beforeEachHelper() {
-  let detection:any = undefined
-  cy.intercept("GET", "**/" + apiUri.specsDetection.replace("/api/","")+ "*",(req) => {
-    req.alias = "detection"
-   
-      req.reply( buildReply())
-    
-  });
-  cy.intercept("GET", "**/" + apiUri.configuration.replace("/api/",""),  {
+  let detection: any = undefined;
+  cy.intercept(
+    "GET",
+    "**/" + apiUri.specsDetection.replace("/api/", "") + "*",
+    (req) => {
+      req.alias = "detection";
+
+      req.reply(buildReply());
+    },
+  );
+  cy.intercept("GET", "**/" + apiUri.configuration.replace("/api/", ""), {
     fixture: "configuration.json",
-   });
-  cy.intercept("GET", "**/" + apiUri.bus.replace("/api/","") + "*", {
-    fixture: "bus.json",
-  });  
-  cy.intercept("GET", "**/" + apiUri.slaves.replace("/api/","")+ "*", {
-    fixture: "slaves.json",
-  });  
-  cy.intercept("GET", "**/" + apiUri.specifications.replace("/api/","")+ "*", {
-    fixture: "specifications.json",
   });
+  cy.intercept("GET", "**/" + apiUri.bus.replace("/api/", "") + "*", {
+    fixture: "bus.json",
+  });
+  cy.intercept(
+    "GET",
+    "**/" + apiUri.specifications.replace("/api/", "") + "*",
+    {
+      fixture: "specifications.json",
+    },
+  );
   // This configures the rootUrl for /api... calls
   // they need to be relative in ingress scenarios,
   // but they must be absolute for cypress tests
@@ -121,15 +134,18 @@ export function beforeEachHelper() {
   });
   cy.mount(SelectSlaveComponent, {
     imports: [NoopAnimationsModule],
-    providers: [provideHttpClient(withInterceptorsFromDi()), provideRouter([]),{
+    providers: [
+      provideHttpClient(withInterceptorsFromDi()),
+      provideRouter([]),
+      {
         provide: ActivatedRoute,
         useValue: {
-          params: from([{busid: 1}]),
+          params: from([{ busid: 1 }]),
         },
-      },],
+      },
+    ],
     componentProperties: {
-        slaveidEventEmitter:ev
+      slaveidEventEmitter: ev,
     },
   });
 }
-
